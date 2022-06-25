a5a5f6383d20c3a30a5f06851d213cbbcf7604a8
ondie2157 <bac22001@byui.edu>
2022-06-25 22:43:11 +0800

    update create weather

--- chamber2/js/windChill.js
+++ chamber2/js/windChill.js
@@ -1,17 +1,37 @@
-let t = parseFloat(document.querySelector("#t").textContent);
-let s = parseFloat(document.querySelector("#s").textContent);
-let windchill = "";
 
-if (t <= 50 && s > 3) {
-  windchill = windChill(t, s);
-  windchill = `${windchill}&#176;F`;
-} 
-else {
-  windchill = "N/A";
-}
-// output
-document.querySelector("#w").innerHTML = windchill;
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=san+jose+del+monte%2C+bulacan%2C+philippines&units=Imperial&appid=499b2f2e9f946f9bcd69ab88f78113d2";
 
-function windChill(temp, speed) {
-  return "SUCCESS";
-}
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        console.log(jsonObject);
+
+        const iconURL = `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`;
+
+        let temp = jsonObject.main.temp;
+        let speed = jsonObject.wind.speed;
+
+        document.querySelector('.cityName').textContent = jsonObject.name;
+        document.querySelector('figcaption').textContent = jsonObject.weather[0].description;
+        document.querySelector('#weatherIcon').setAttribute('src', iconURL);
+        document.querySelector('#weatherIcon').setAttribute('alt', jsonObject.weather[0].description);
+        document.querySelector('.temp').innerHTML = `Temperature: ${temp} &deg;F`;
+        document.querySelector('.wind-speed').textContent = `Wind Speed: ${speed} mph`;
+        document.querySelector('.humidityDiv').textContent = `Humidity: ${jsonObject.main.humidity}`;
+
+        let windChill = computeWindChill(temp, speed);
+
+        document.querySelector('.wind-chill').textContent = `Wind Chill: ${windChill}`;
+
+        
+    });
+
+    function computeWindChill(t, s) {
+        windchill = Math.round((35.74 + 0.6215 * t - 35.75 * s * 0.16 + 0.4275 * t * s * 0.16) * 100 / 100);
+
+        if (t <= 50 && s > 3) {
+            return windchill
+        } else {
+            return N/A
+        }
+    }
